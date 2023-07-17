acdc1e8ebefee3f23acd96db7844cf1c5e92ee5e
Marat <104265985+Maratq@users.noreply.github.com>
2023-07-17 18:54:39 +0400

    create models

---
+++ src/app/models.py
@@ -0,0 +1,13 @@
+from tortoise import fields
+from tortoise.models import Model
+from datetime import date
+
+
+class Entry(Model):
+    id = fields.IntField(pk=True)
+    date = fields.DateField(default=date.today)
+    cargo_type = fields.CharField(max_length=255)
+    rate = fields.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        table = "Entry"
